c8501df735537b3bea94f0c101357a3fc5ca859f
kruigi <dskumar2651@gmail.com>
2026-02-03 08:55:06 -0800

    Create For_loop_practice.py

---
+++ For_loop_practice.py
@@ -0,0 +1,19 @@
+def main():
+  print("Starting The Code Challenge")
+  y = 0
+  my_list = [1,2,3,4,5,6,7,8,9]
+  for x in my_list:
+       print(x)
+
+  print("Starting while loop part Of Code Challenge")
+  while y != 10:
+      print(y)
+      y = y+1
+  print('Enter Your Code Here:')
+  z = 42
+  for x in range(3):
+    print(z)
+  print("Ending loops Of Code Challenge")
+  print("Ending The Code Challenge")
+
+main()
