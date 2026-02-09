390e14e3876e3a5cea8f5dc7aacb6b3ea1efae79
kruigi <dskumar2651@gmail.com>
2026-02-09 10:18:28 -0800

    Create Babylon_5_lists.py

---
+++ Babylon_5_lists.py
@@ -0,0 +1,20 @@
+# Python Lists
+x = 42
+print(x)
+print(type(x))
+# re-assign x to be a list with the following elements:
+# 1,3,5,23,9,14
+x = [1,3,5,23,9,14]
+print(x)
+print(type(x))
+my_list = ['hi there',25,'what?',2.5,67]
+print(my_list)
+blank_list=[]
+print(blank_list)
+babylon_5_races = ["Centauri", "Minbari", "Narn", "Human",  "Shadow", "Vorlon"]
+print(babylon_5_races)
+print(babylon_5_races[0])
+print(babylon_5_races[3])
+print(len(babylon_5_races))
+print(babylon_5_races[-1])
+print(babylon_5_races[-2])
