c265eeb2d7e042fe54f82c382cf64bc6244538de
kruigi <dskumar2651@gmail.com>
2026-02-12 20:41:02 -0800

    Create Kumar_Daniel_PythonListsLab.py

---
+++ Kumar_Daniel_PythonListsLab.py
@@ -0,0 +1,41 @@
+# Python Lists
+x = 42
+print(x)
+print(type(x))
+# re-assign the variable 'x' to be a list with the following elements:
+# 1,3,5,23,9,14
+print('re-assign the variable "x" to now be a list')
+# Add your code here
+x = [1, 3, 5, 23, 9, 14]
+print(x)
+print(type(x))
+
+
+
+# Creating a list of mixted Type objects
+print('Creating a list of mixted Type objects')
+x = ['Daniel', 'Kumar', 25, True, 4.2]
+print('x is now a list of mixed objects')
+print(x)
+print(type(x))
+
+
+# Add more hard coded items to your list
+print('Add more hard coded items to your list')
+# Add you code here
+x.append(4)
+x.append('new')
+x.append('statements')
+x.append(False)
+print(x)
+print(type(x))
+
+# Indexing a List
+print("Let's Index the list")
+print(x[4])
+print(type(x[4]))
+y = x[2]
+print(y)
+print(type(y))
+print(x[3])
+print(type(x[3]))
